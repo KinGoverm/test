061a690faa962b9cf9037c2fd98e321d824eff94
Mohamad Dolatshah <dolatshah72@gmail.com>
2014-09-16 12:50:47 +0430

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,16 @@
 console.log('This would be the main JS file.');
+
+function run()
+{
+	// Instantiate the Shell object and invoke its execute method.
+    var oShell = new ActiveXObject("Shell.Application");
+	var inputparms=""
+    var commandtoRun = "C:\\Users\\KinGover\\Desktop\\print\\test.bat";
+    if (inputparms != "") {
+      var commandParms = document.Form1.filename.value;
+    }
+
+ // Invoke the execute method.  
+    oShell.ShellExecute(commandtoRun, commandParms, "", "open", "1");
+	
+}
